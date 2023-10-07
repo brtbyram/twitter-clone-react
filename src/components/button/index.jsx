ddf6c2bb7502607d65ffea0f3bc9f17839ef3742
Berat Murathan Bayram <brtbyram.61@gmail.com>
2023-10-07 22:09:39 +0300

    5- Sağ sidebar tamamlandı

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,17 +2,18 @@ import { createElement } from "react"
 import classNames from "classnames"
 import PropTypes from "prop-types"
 
-export default function Button({ as, size, variant, children }) {
+export default function Button({ as, size, variant, className, children, ...props }) {
     return createElement(as, {
         type: 'button',
-        className: classNames("rounded-full flex items-center text-#fff justify-center font-bold transition-all", {
+        className: classNames("rounded-full flex items-center justify-center font-bold transition-all", {
             "px-4 h-8 text-sm": size === 'small',
             "px-4 h-9": size === 'normal',
             "px-4 text-[1.063rem] h-[3.25rem] w-full": size === 'large',
             "bg-[#1d9bf0] hover:opacity-90 text-#fff": variant === 'primary',
-            "bg-#fff text-[#1d9bf0]": variant === 'white',
-			"border border-[#b4b4b4] hover:border-[#67070f] hover:bg-[#f4212e1a] hover:text-[#f4212e]": variant === 'white-outline'
-        })
+            "bg-white text-black": variant === 'white',
+			"border border-[#b4b4b4] hover:border-[#67070f] hover:bg-[#f4212e1a] hover:text-[#f4212e]": variant === 'white-outline',
+            [className]: true
+        }),...props
     }, children)
 }
 
@@ -20,6 +21,8 @@ Button.propTypes = {
     as: PropTypes.any,
     size: PropTypes.oneOf(['small','normal','large']),
     variant: PropTypes.oneOf(['primary', 'white', 'white-outline']),
+    props: PropTypes.object,
+    className: PropTypes.string
 }
 
 Button.defaultProps = {
---
+++ src/components/user-card/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Button from "../button";
+
+export default function UserCard({ user }) {
+
+    const [following, setFollowing] = useState(false)
+
+    return (
+        <button className="py-3 px-4 flex items-center gap-3 transition-colors hover:bg-white/[0.03]">
+            <img src={user.avatar} className="w-10 h-10 rounded-full object-cover" alt="" />
+            <div className="flex flex-1 flex-col text-left">
+                <div className="text-[15px] text-[#e7e9ea] leading-7 font-bold">
+                    {user.fullName}
+                </div>
+                <div className="text-[15px] text-[#71767b] ">@{user.username}</div>
+            </div>
+            <div>
+                {following ? (
+                    <Button variant="white-outline" size="small" onClick={() => setFollowing(false)} className="whitespace-nowrap group">
+                        <div className="flex group-hover:hidden">Takip ediliyor</div>
+                        <div className="hidden group-hover:flex">Takibi bırak</div>
+                    </Button>
+                ) : (
+                    <Button variant="white" size="small" onClick={() => setFollowing(true)}>Takip et</Button>
+                )}
+            </div>
+        </button>
+    )
+}
